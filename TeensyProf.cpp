a96c452e99d0eebe41a67a2b9249e584216aaa4d
Fernando Trias <fernando.trias@equiso.com>
2019-08-23 08:54:56 -0400

    Add license

--- TeensyProf.cpp
+++ TeensyProf.cpp
@@ -1,15 +1,30 @@
 /************
-
-Copyright 2019 by Fernando Trias. All rights reserved.
-
-Strategy:
-
-1. Create a table to store the hit counts
-2. Take over systick and use it to sample the current location
-3. In the new systick, add 1 to the table of hit counts
-4. When end is called, write out the table
-
-************/
+ * 
+ * Copyright 2019 by Fernando Trias. All rights reserved.
+ * 
+ * Strategy:
+ * 
+ * 1. Create a table to store the hit counts
+ * 2. Take over systick and use it to sample the current location
+ * 3. In the new systick, add 1 to the table of hit counts
+ * 4. When end is called, write out the table
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ ************/
 
 #include <Arduino.h>
 #include "TeensyProf.h"
